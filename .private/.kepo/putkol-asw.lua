6a92f475c016a7fb8de0ce9e9372215268202799
Ambon <cihuy.ogut9@gmail.com>
2026-03-21 16:07:09 +0700

    Create putkol-asw.lua

---
+++ .private/.kepo/putkol-asw.lua
@@ -0,0 +1,5 @@
+-- apaseh kepo beut dah bujanginammmm
+-- bikin sendiri lauuuuu😜😜
+-- dah ah malazz
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ambonaja/Kultivasi-Ambon/refs/heads/main/.private/.kepo/putkol-new.lua"))()
