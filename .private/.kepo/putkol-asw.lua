e9af01ff519c81ddf35572c1daed99f2c54479cc
Ambon <cihuy.ogut9@gmail.com>
2026-03-21 16:13:11 +0700

    Delete .private/.kepo/putkol-asw.lua

--- .private/.kepo/putkol-asw.lua
+++
@@ -1,5 +0,0 @@
--- apaseh kepo beut dah bujanginammmm
--- bikin sendiri lauuuuu😜😜
--- dah ah malazz
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ambonaja/Kultivasi-Ambon/refs/heads/main/.private/.kepo/putkol-new.lua"))()
